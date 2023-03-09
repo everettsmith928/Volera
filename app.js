c743884cb479fec02536e40645a409c7934a3f37
GarrettSmithVolera <GarrettSmith@volera.io>
2023-03-09 16:42:58 -0700

    Code and file import
    Transferred code and files from rough draft project

--- app.js
+++ app.js
@@ -2,18 +2,41 @@ const express = require('express');
 
 const app = express();
 
+//Start server
 app.listen(3000);
 
+//Use EJS
 app.set('view engine', 'ejs');
+
+//Share public files
+app.use(express.static('public'));
+
+//Grab form data
+app.use(express.urlencoded({extended: true}));
 
 //Index
 app.get('/', (req, res) => {
-    res.render('index', {name: 'Garrett'});
+    res.render('index');
 });
 
 //About
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+//Roadmap
+app.get('/roadmap', (req, res) => {
+    res.render('roadmap');
+})
 
 //Signup
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+})
 
 //Default
 app.use((req, res) => {
