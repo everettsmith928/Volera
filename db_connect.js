568f488917264bacee7bfa77bea8b36e6b2455a9
GarrettSmithVolera <GarrettSmith@volera.io>
2023-03-09 17:25:17 -0700

    svg files

--- db_connect.js
+++ db_connect.js
@@ -1,9 +1,10 @@
 // Server File
-var mysql = require('mysql2');
-var fs = require('fs');
+const mysql = require('mysql2');
+const fs = require('fs');
 
 // Database Connection
- var con = mysql.createConnection({
+const con = (req, res) => {
+  mysql.createConnection({
 	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PASSWORD,
@@ -13,10 +14,15 @@ var fs = require('fs');
         ca : fs.readFileSync('ca-certificate.crt'),
         rejectUnauthorized: false
     }
-});
+  })
 
-// Connect to DB
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  // Connect to DB
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+};
+
+module.exports = {
+  con
+}
